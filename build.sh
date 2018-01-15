b8fd6bd01594eb94058f6fea7669a9570650266a
Eduardo Noyer <e.noyer.silva@gmail.com>
2018-01-15 10:18:52 -0600

    Updated build.sh
    Now uses $LOUP_WORKING_DIR , it's defined in kernel build.sh script.

--- build.sh
+++ build.sh
@@ -5,22 +5,22 @@ ANDROID_VERSION=$2
 FILE_NAME="Loup.Kernel-v${LOUP_VERSION}-${ANDROID_VERSION}-${CURRENT_DATE}.zip"
 
 # Delete old stuff.
-rm ~/CAF/AnyKernel2/*.zip
-if [ -f ~/CAF/AnyKernel2/modules/system/lib/modules/wlan.ko ]; then
-    rm ~/CAF/AnyKernel2/modules/system/lib/modules/wlan.ko
+rm $LOUP_WORKING_DIR/AnyKernel2/*.zip
+if [ -f $LOUP_WORKING_DIR/AnyKernel2/modules/system/lib/modules/wlan.ko ]; then
+    rm $LOUP_WORKING_DIR/AnyKernel2/modules/system/lib/modules/wlan.ko
 fi
 
 # Start building anykernel zip.
-cp ./arch/arm64/boot/Image.gz-dtb ~/CAF/AnyKernel2/zImage
-find . -type f -name "wlan.ko" -exec cp -fv {} ~/CAF/AnyKernel2/modules/system/lib/modules/. \;
+cp ./arch/arm64/boot/Image.gz-dtb $LOUP_WORKING_DIR/AnyKernel2/zImage
+find . -type f -name "wlan.ko" -exec cp -fv {} $LOUP_WORKING_DIR/AnyKernel2/modules/system/lib/modules/. \;
 
 # Update kernel version
-cp ~/CAF/AnyKernel2/anykernel-template.sh ~/CAF/AnyKernel2/anykernel.sh
-sed -i -e "s/LOUP_VERSION/$LOUP_VERSION/g" ~/CAF/AnyKernel2/anykernel.sh
-sed -i -e "s/ANDROID_VERSION/$ANDROID_VERSION/g" ~/CAF/AnyKernel2/anykernel.sh
+cp $LOUP_WORKING_DIR/AnyKernel2/anykernel-template.sh $LOUP_WORKING_DIR/AnyKernel2/anykernel.sh
+sed -i -e "s/LOUP_VERSION/$LOUP_VERSION/g"$LOUP_WORKING_DIR/AnyKernel2/anykernel.sh
+sed -i -e "s/ANDROID_VERSION/$ANDROID_VERSION/g" $LOUP_WORKING_DIR/AnyKernel2/anykernel.sh
 
 # Zip it!.
-pushd ~/CAF/AnyKernel2
+pushd $LOUP_WORKING_DIR/AnyKernel2
 zip -r9 ${FILE_NAME} * -x build.sh README.md anykernel-template.sh
 popd
 
